d504d1be491e479ab75a8bcd903913d4c9807b87
Luiz Gustavo <luiz.gustavo@unemat.br>
2022-08-31 11:43:30 -0400

    feat: cria estrutura de tema personalizado do mui na pagina App

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
-
+import { ThemeProvider, createTheme } from "@mui/material"
+import React from "react"
+import Home from "./page/home"
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <ThemeProvider theme={theme}>
+      <Home />
+    </ThemeProvider>
+  )
 }
 
-export default App;
+const theme = createTheme({
+  spacing: 4,
+  palette: {
+    primary: {
+      main: "#f44336",
+    },
+    secondary: {
+      main: "#3f51b5",
+    },
+  },
+})
+
+export default App
