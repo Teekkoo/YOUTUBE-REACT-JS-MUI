525b3ea3ae26fdce1934a170c427e7634e23eb82
Luiz Gustavo <luiz.gustavo@unemat.br>
2022-08-31 12:55:48 -0400

    feat: inicia construção do navbar e adiciona botões e icones

--- src/page/home/index.js
+++ src/page/home/index.js
@@ -1,34 +1,41 @@
 import React from "react"
 import { Styles } from "./styled"
-import {
-  AppBar,
-  Box,
-  Button,
-  IconButton,
-  Toolbar,
-  Typography,
-} from "@mui/material"
+import { AppBar, Box, Button, IconButton, Toolbar } from "@mui/material"
 import MenuIcon from "@mui/icons-material/Menu"
+import { AccountCircle, Apps, MoreVert, VideoCall } from "@mui/icons-material"
 
 const Home = () => {
   const styled = Styles()
   return (
     <Box className={styled.container}>
-      <AppBar color="inherit">
+      <AppBar color="inherit" className={styled.appBar}>
         <Toolbar>
           <IconButton
             size="large"
             edge="start"
             color="inherit"
             aria-label="menu"
-            sx={{ mr: 2 }}
+            className={styled.iconMenu}
           >
             <MenuIcon />
           </IconButton>
-          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-            News
-          </Typography>
-          <Button color="inherit">Login</Button>
+          <Box className={styled.grow} />
+          <IconButton size="large" color="inherit" className={styled.icon}>
+            <VideoCall />
+          </IconButton>
+          <IconButton size="large" color="inherit" className={styled.icon}>
+            <Apps />
+          </IconButton>
+          <IconButton size="large" color="inherit" className={styled.icon}>
+            <MoreVert />
+          </IconButton>
+          <Button
+            variant="outlined"
+            color="secondary"
+            startIcon={<AccountCircle />}
+          >
+            Fazer Login
+          </Button>
         </Toolbar>
       </AppBar>
     </Box>
--- src/page/home/styled.js
+++ src/page/home/styled.js
@@ -4,4 +4,17 @@ export const Styles = makeStyles((theme) => ({
   container: {
     height: "100vh",
   },
+  appBar: {
+    boxShadow: "none !important",
+  },
+  iconMenu: {
+    paddingRight: theme.spacing(5) + "!important", //define o espaçamento como 4 * 5
+    paddingLeft: theme.spacing(6) + "!important", //define o espaçamento como 4 * 6
+  },
+  icon: {
+    paddingRight: theme.spacing(5) + "!important", //define o espaçamento como 4 * 5
+  },
+  grow: {
+    flexGrow: 1,
+  },
 }))
