be4fabec46e05f6e3fd7e5bc46c2bb6b37a6b28b
Luiz Gustavo <luiz.gustavo@unemat.br>
2022-09-01 14:11:23 -0400

    feat: inicia codagem do menu lateral com o componente drawer do mui

--- src/page/home/index.js
+++ src/page/home/index.js
@@ -1,11 +1,40 @@
 import React from "react"
 import { Styles } from "./styled"
-import { AppBar, Box, Button, IconButton, Toolbar } from "@mui/material"
+import {
+  AppBar,
+  Box,
+  Button,
+  CardMedia,
+  Drawer,
+  IconButton,
+  Toolbar,
+  Divider,
+  List,
+  ListItem,
+  ListItemButton,
+  ListItemIcon,
+  ListItemText,
+  Typography,
+  ListSubheader,
+} from "@mui/material"
 import MenuIcon from "@mui/icons-material/Menu"
-import { AccountCircle, Apps, MoreVert, VideoCall } from "@mui/icons-material"
+import {
+  AccountCircle,
+  Apps,
+  History,
+  MoreVert,
+  Subscriptions,
+  VideoCall,
+  VideoLibrary,
+  Whatshot,
+} from "@mui/icons-material"
+import HomeIcon from "@mui/icons-material/Home"
+
+const drawerWidth = 240
 
 const Home = () => {
   const styled = Styles()
+
   return (
     <Box className={styled.container}>
       <AppBar color="inherit" className={styled.appBar}>
@@ -19,6 +48,12 @@ const Home = () => {
           >
             <MenuIcon />
           </IconButton>
+          <CardMedia
+            component="img"
+            className={styled.logo}
+            image="/images/preto.png"
+            alt="Logo"
+          />
           <Box className={styled.grow} />
           <IconButton size="large" color="inherit" className={styled.icon}>
             <VideoCall />
@@ -38,6 +73,234 @@ const Home = () => {
           </Button>
         </Toolbar>
       </AppBar>
+      <Drawer
+        sx={{
+          width: drawerWidth,
+          flexShrink: 0,
+          "& .MuiDrawer-paper": {
+            width: drawerWidth,
+            boxSizing: "border-box",
+            borderRight: "none",
+          },
+        }}
+        variant="permanent"
+        anchor="left"
+      >
+        <Toolbar />
+
+        <List>
+          <ListItem classes={{ root: styled.listItem }}>
+            <ListItemButton>
+              <ListItemIcon>
+                <HomeIcon />
+              </ListItemIcon>
+              <ListItemText
+                classes={{
+                  primary: styled.listItemText,
+                }}
+                primary={"Inicio"}
+              />
+            </ListItemButton>
+          </ListItem>
+          <ListItem classes={{ root: styled.listItem }}>
+            <ListItemButton>
+              <ListItemIcon>
+                <Whatshot />
+              </ListItemIcon>
+              <ListItemText
+                classes={{
+                  primary: styled.listItemText,
+                }}
+                primary={"Em Alta"}
+              />
+            </ListItemButton>
+          </ListItem>
+          <ListItem classes={{ root: styled.listItem }}>
+            <ListItemButton>
+              <ListItemIcon>
+                <Subscriptions />
+              </ListItemIcon>
+              <ListItemText
+                classes={{
+                  primary: styled.listItemText,
+                }}
+                primary={"Inscrições"}
+              />
+            </ListItemButton>
+          </ListItem>
+        </List>
+        <Divider />
+        <List>
+          <ListItem classes={{ root: styled.listItem }}>
+            <ListItemButton>
+              <ListItemIcon>
+                <VideoLibrary />
+              </ListItemIcon>
+              <ListItemText
+                classes={{
+                  primary: styled.listItemText,
+                }}
+                primary={"Biblioteca"}
+              />
+            </ListItemButton>
+          </ListItem>
+          <ListItem classes={{ root: styled.listItem }}>
+            <ListItemButton>
+              <ListItemIcon>
+                <History />
+              </ListItemIcon>
+              <ListItemText
+                classes={{
+                  primary: styled.listItemText,
+                }}
+                primary={"Historico"}
+              />
+            </ListItemButton>
+          </ListItem>
+        </List>
+        <Divider />
+        <Box p={7}>
+          <Typography variant="body2">
+            Faça login para curtir videos, comentar e se inscrever.
+          </Typography>
+
+          <Box mt={2}>
+            <Button
+              size="small"
+              variant="outlined"
+              color="secondary"
+              startIcon={<AccountCircle />}
+            >
+              Fazer Login
+            </Button>
+          </Box>
+        </Box>
+        <Divider />
+        <List>
+          <ListSubheader>O melhor do Youtube</ListSubheader>
+          <ListItem classes={{ root: styled.listItem }}>
+            <ListItemButton>
+              <ListItemIcon>
+                <HomeIcon />
+              </ListItemIcon>
+              <ListItemText
+                classes={{
+                  primary: styled.listItemText,
+                }}
+                primary={"Musica"}
+              />
+            </ListItemButton>
+          </ListItem>
+          <ListItem classes={{ root: styled.listItem }}>
+            <ListItemButton>
+              <ListItemIcon>
+                <Whatshot />
+              </ListItemIcon>
+              <ListItemText
+                classes={{
+                  primary: styled.listItemText,
+                }}
+                primary={"Esportes"}
+              />
+            </ListItemButton>
+          </ListItem>
+          <ListItem classes={{ root: styled.listItem }}>
+            <ListItemButton>
+              <ListItemIcon>
+                <Subscriptions />
+              </ListItemIcon>
+              <ListItemText
+                classes={{
+                  primary: styled.listItemText,
+                }}
+                primary={"Jogos"}
+              />
+            </ListItemButton>
+          </ListItem>
+          <ListItem classes={{ root: styled.listItem }}>
+            <ListItemButton>
+              <ListItemIcon>
+                <HomeIcon />
+              </ListItemIcon>
+              <ListItemText
+                classes={{
+                  primary: styled.listItemText,
+                }}
+                primary={"Filme"}
+              />
+            </ListItemButton>
+          </ListItem>
+          <ListItem classes={{ root: styled.listItem }}>
+            <ListItemButton>
+              <ListItemIcon>
+                <HomeIcon />
+              </ListItemIcon>
+              <ListItemText
+                classes={{
+                  primary: styled.listItemText,
+                }}
+                primary={"Noticias"}
+              />
+            </ListItemButton>
+          </ListItem>
+          <ListItem classes={{ root: styled.listItem }}>
+            <ListItemButton>
+              <ListItemIcon>
+                <HomeIcon />
+              </ListItemIcon>
+              <ListItemText
+                classes={{
+                  primary: styled.listItemText,
+                }}
+                primary={"Ao Vivo"}
+              />
+            </ListItemButton>
+          </ListItem>
+          <ListItem classes={{ root: styled.listItem }}>
+            <ListItemButton>
+              <ListItemIcon>
+                <HomeIcon />
+              </ListItemIcon>
+              <ListItemText
+                classes={{
+                  primary: styled.listItemText,
+                }}
+                primary={"Destaques"}
+              />
+            </ListItemButton>
+          </ListItem>
+          <ListItem classes={{ root: styled.listItem }}>
+            <ListItemButton>
+              <ListItemIcon>
+                <HomeIcon />
+              </ListItemIcon>
+              <ListItemText
+                classes={{
+                  primary: styled.listItemText,
+                }}
+                primary={"Video em 360º"}
+              />
+            </ListItemButton>
+          </ListItem>
+        </List>
+        <Divider />
+        <List>
+          <ListItem classes={{ root: styled.listItem }}>
+            <ListItemButton>
+              <ListItemIcon>
+                <HomeIcon />
+              </ListItemIcon>
+              <ListItemText
+                classes={{
+                  primary: styled.listItemText,
+                }}
+                primary={"Procurar canais"}
+              />
+            </ListItemButton>
+          </ListItem>
+        </List>
+        <Divider />
+      </Drawer>
     </Box>
   )
 }
--- src/page/home/styled.js
+++ src/page/home/styled.js
@@ -6,15 +6,27 @@ export const Styles = makeStyles((theme) => ({
   },
   appBar: {
     boxShadow: "none !important",
+    zIndex: theme.zIndex.drawer + 1 + "!important",
   },
   iconMenu: {
     paddingRight: theme.spacing(5) + "!important", //define o espaçamento como 4 * 5
     paddingLeft: theme.spacing(6) + "!important", //define o espaçamento como 4 * 6
+  },
+  logo: {
+    height: 25,
+    width: "auto !important",
   },
   icon: {
     paddingRight: theme.spacing(5) + "!important", //define o espaçamento como 4 * 5
   },
   grow: {
     flexGrow: 1,
+  },
+  listItemText: {
+    fontSize: "14px ",
+  },
+  listItem: {
+    paddingTop: "4px !important",
+    paddingBottom: "4px !important",
   },
 }))
